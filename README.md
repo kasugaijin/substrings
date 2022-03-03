c668e06a66d3b995ae058762d5f990e5a443c29b
Ben <robinsonbena@gmail.com>
2022-03-03 09:57:07 -0800

    refactored to be more efficient

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # substrings
 This Ruby program takes a user-defined string and library and then returns a hash containing the count of each string within the library. If you have Ruby installed locally you can download ruby.rb and run it, or view it online at: https://replit.com/@BenJ8/Substring#main.rb
+
+I later reviewed the solutions of others and realised I could have been more efficient so refactored to cut code lines down by a third while maintaining readability. 
--- ruby.rb
+++ ruby.rb
@@ -1,34 +1,23 @@
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
-#Convert string to array
-#iterate over array and push to new array all items that match a word in dictionary
+#convert string to downcase then to an array.
+#then for each element loop through dictionary and if included determine if hash has key
+#if key exists, iterate the value, if does not exist, add key with value 1
+#if you try to iterate to an empty key/value pair you get the nil noMethodError #askmehowIknow
 def substring(string, dictionary)
-  string.downcase!
-  string_array = string.split
-  match_array = []
-  count_array = []
-  result = {}
-  
-  #if substring from dictionary is in 'string' push substring to match_array
-  #count the number of times each dictionary substring occurs in string and push to count_array, remove zeros
-  dictionary.each do |i|  
-    if string_array.any? {|s| s.include?(i)}
-    match_array.push(i)
+  hash = {}
+  string_array = string.downcase.split
+  string_array.each do |i|
+    dictionary.each do |s|
+     if i.include?(s)
+        if hash.has_key?(s)
+          hash[s] += 1
+        else hash[s] = 1
+        end
+      end
     end
-    count = string_array.count {|s| s.include?(i)}
-    count_array.push(count)
-    count_array.delete(0)
   end
-  
-  #final result is a hash made from two equal length arrays, match_array and count_array
-  #where one array is the key and the other is the value
-  #this is a convenient shorthand - thank you Ruby!
-  result = match_array.zip(count_array).to_h
-
-  p "these are the substrings from dictionary that match string #{match_array}"
-  p "these matches occur this many times #{count_array}"
-  p "the final hash is #{result}"
+  p hash
 end
 
-substring("below", dictionary)
-substring("Howdy partner, sit down! How's it going", dictionary)
+substring("below low low cuthbert blows his ownership hornbill", dictionary)
